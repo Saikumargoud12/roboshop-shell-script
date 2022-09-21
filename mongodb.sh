891759edd2040dde6c5fda3231402ece445e956b
saikumargoud12 <sabbagauni@gmail.com>
2022-09-21 19:47:11 -0400

    adding mondodb installation steps

--- mongodb.sh
+++ mongodb.sh
@@ -1,4 +1,4 @@
-LOG_FILE=$LOG_FILE
+LOG_FILE=/tmp/mongodb
 echo "setting up the mongodb repo file"
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo>>/$LOG_FILE
 echo status=$?
@@ -6,6 +6,6 @@ echo "installing mongodb server"
 yum install -y mongodb-org>>$LOG_FILE
 echo status=$?
 echo "starting mongodb service"
- systemctl enable mongod>>$LOG_FILE
- systemctl start mongod>>$LOG_FILE
- echo status=$?
+systemctl enable mongod>>$LOG_FILE
+systemctl start mongod>>$LOG_FILE
+echo status=$?
